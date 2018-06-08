57a4fa12e1a8546a8f0a9b5230427904fd4bb8eb
a-d-spina-student <a.d.spina@student.rug.nl>
2018-06-08 18:30:39 +0200

    Added download for probe script option

--- cloudComputeCostWaste/routes/downloadProbe.js
+++ cloudComputeCostWaste/routes/downloadProbe.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
+const fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index');
+    res.download('testCurl.sh');
 });
 module.exports = router;
