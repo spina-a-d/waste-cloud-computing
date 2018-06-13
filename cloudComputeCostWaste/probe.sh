597f9c545fdd98e541767da7e90502eb49b3e204
a-d-spina-student <a.d.spina@student.rug.nl>
2018-06-13 18:08:17 +0200

    Changed stuff for probe to test on vm

--- cloudComputeCostWaste/probe.sh
+++ cloudComputeCostWaste/probe.sh
@@ -3,7 +3,7 @@
 USER_ID="5b1818145f2f3b51b3c5b0f4" #Probably not necessary and will probably be removed
 APP_TOKEN="5b1b97d039cabc0fe281c6bf" #Token provided which uniquely identifies application
 IMAGE_TOKEN="5b1aa63dcfcc4a406d04acdd" #Token provided which uniquely identifies image
-DESTINATION="localhost:3000"
+DESTINATION="ec2-54-93-254-73.eu-central-1.compute.amazonaws.com:3000"
 PORT=3000 #Port on which app traffic takes place
 PING_RATE=2 #how long between probe pings (use at least 1)
 #IF on public cloud 
