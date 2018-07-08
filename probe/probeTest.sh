b3ed971dc035e02b23802269c6e6142f8f7da06f
a-d-spina-student <a.d.spina@student.rug.nl>
2018-07-08 18:09:50 +0200

    made new probetest

--- probe/probeTest.sh
+++ probe/probeTest.sh
@@ -1,7 +1,57 @@
 #!/bin/bash
 
+#https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+
+##CONFIG##
+USER_ID="5b1818145f2f3b51b3c5b0f4" #Probably not necessary and will probably be removed
+IMAGE_TOKEN="5b40f095193cc743cfe8dc22" #Token provided which uniquely identifies image
+DESTINATION="http://www.universalcloudmonitoring.com"
+PORT=3000 #Port on which app traffic takes place
+PING_RATE=3 #how long between probe pings (use at least 1)
+#IF on public cloud 
+INSTANCE_TYPE="5b1ece31617c3e4c95151ed8"
+
+function send_data (){
+	curl --fail --header "Content-Type: application/json" \
+	  	--header 'Expect:' \
+	  	--request POST \
+	  	--data "$1" \
+	  	"$DESTINATION/probePost"
+	res=$?
+	return $res
+}
+
 #sends json data via curl to the probePost
 while [ true ]; do
 	sleep 1
-	echo "hello world" >> "tempStorage.json"
+	
+	#Gather new data
+	memFree=9
+    memTotal=9
+    MEMORY=9 #Shows memory usage without buff/cache included
+    diskSize=9
+    diskUsed=9
+    DISK=$(awk "BEGIN {printf \"%.2f\",${diskUsed}/${diskSize}*100}")
+    CPU=9
+    UUID="sdfklj;asdf"
+    TIME=$(date +%s)
+
+	#This is where new data will be extracted and sent
+	newData='{"oauthid": "'$USER_ID'",
+				"image":"'$IMAGE_TOKEN'",  
+				"uuid":"'$UUID'", 
+				"cpu":"'$CPU'", 
+				"mem":"'$MEMORY'", 
+				"disk":"'$DISK'", 
+				"time":"'$TIME'",
+				"instance_type":"'$INSTANCE_TYPE'"
+			}'
+	
+	send_data "$newData"
+	res=$?
+
+	if test "$res" != "0"; then
+	   echo $newData >> "tempStorage.json"
+	fi
+	:
 done
