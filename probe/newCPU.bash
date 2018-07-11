aab8c6a11b8e5ec981602e4c08da46b2c36f941a
a-d-spina-student <a.d.spina@student.rug.nl>
2018-07-11 17:12:43 +0200

    Added probe for RHEL

--- probe/newCPU.bash
+++ probe/newCPU.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-USER_ID=5b1818145f2f3b51b3c5b0f4
-IMAGE_TOKEN=5b461dea998e275f28faba88
-DESTINATION=localhost:3000
-PORT=3000
-PING_RATE=5
-INSTANCE_TYPE=5b1ecf446bcc0a4d5d81ae29
+#https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+
+##CONFIG##
+USER_ID="5b1818145f2f3b51b3c5b0f4" #Probably not necessary and will probably be removed
+IMAGE_TOKEN="5b40f095193cc743cfe8dc22" #Token provided which uniquely identifies image
+DESTINATION="http://www.universalcloudmonitoring.com"
+PORT=3000 #Port on which app traffic takes place
+PING_RATE=5 #how long between probe pings (use at least 1)
+#IF on public cloud 
+INSTANCE_TYPE="5b1ece31617c3e4c95151ed8"
 
 ##CODE - Do not modify##
 function CPU_usage(){
@@ -118,8 +122,7 @@ while [ true ]; do
     TIME=$(date +%s)
 
 	#This is where new data will be extracted and sent
-	newData='{"oauthid": "'$USER_ID'", 
-				"app":"'$APP_TOKEN'", 
+	newData='{"oauthid": "'$USER_ID'",
 				"image":"'$IMAGE_TOKEN'",  
 				"uuid":"'$UUID'", 
 				"cpu":"'$CPU'", 
