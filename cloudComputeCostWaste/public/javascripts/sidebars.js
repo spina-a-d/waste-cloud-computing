f4bd07f87a5aaef5055abdb6eca6e70c67b35c12
a-d-spina-student <a.d.spina@student.rug.nl>
2018-10-02 21:33:15 +0200

    Added inline script

--- cloudComputeCostWaste/public/javascripts/sidebars.js
+++ cloudComputeCostWaste/public/javascripts/sidebars.js
@@ -73,20 +73,3 @@ function closeContact() {
 function closeLogin(){
 	document.getElementById("myLoginBar").style.top = "-300px";
 }
-
-var acc = document.getElementsByClassName("accordion");
-for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-} 
